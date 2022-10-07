57c4ad8cfc49caf1287203a9794c45c9719fd144
Guilherme Duarte <gduarte@outlook.com>
2022-10-07 18:38:12 -0300

    skeleton test + start fetch usage

--- js/script.js
+++ js/script.js
@@ -23,15 +23,12 @@ const searchBtn = document.querySelector(".search-icon");
 const searchMobile = document.querySelector("#search-mobile");
 const logo = document.querySelector(".font-1-logo");
 
-console.log(window);
-
 searchBtn.addEventListener("click", (e) => {
   searchBtn.classList.add("active");
   searchMobile.classList.add("active");
   searchMobile.focus();
 
   if (window.innerWidth <= 767) {
-    console.log("aaaa");
     logo.style.display = "none";
   }
 });
@@ -41,3 +38,35 @@ searchMobile.addEventListener("focusout", () => {
   searchMobile.classList.remove("active");
   logo.style.display = "block";
 });
+
+// Single Anime fetch Info from anime
+async function getAnimeInfo() {
+  // Define o parámetro de busca através da URL
+  const queryString = window.location.search;
+  const searchParams = new URLSearchParams(queryString);
+  const animeSearch = searchParams.get("anime");
+
+  // Busca pelo anime usando a API do gogoanime com base no parámetro da URL e retorna o animeId
+  const urlFetchSearch = `https://gogoanime.herokuapp.com/search?keyw=${animeSearch}`;
+  const fetchSearch = await fetch(urlFetchSearch);
+  const fetchSearchJson = await fetchSearch.json();
+  const anime = fetchSearchJson[0].animeId;
+
+  // Utiliza o animeId para buscar as informações do anime.
+  const urlFetchDetails = `https://gogoanime.herokuapp.com/anime-details/${anime}`;
+  const fetchDetails = await fetch(urlFetchDetails);
+  const fetchDetailsJson = await fetchDetails.json();
+  return fetchDetailsJson;
+}
+
+async function addAnimeDetails() {
+  const animeDetails = await getAnimeInfo();
+  const pageAnimeTitle = document.querySelector("#anime-single .video h1");
+  const pageAnimeDuracao = document.querySelector(".sinopse-content li.duracao");
+  const pageAnimeStudio = document.querySelector(".sinopse-content li.studio");
+  const pageAnimeEpisodes = document.querySelector(".sinopse-content li.episodes");
+  const pageAnimePosterWrapper = document.querySelector(".sinopse-img");
+  const sinopse = document.querySelector(".sinopse-paragraph-content");
+}
+
+addAnimeDetails();
